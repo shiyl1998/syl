516548925b2f9dbc45f44fa2441bb568708e245a
shiyl1998 <1530994340@qq.com>
2022-08-04 11:15:14 +0800

    hello-1

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("hello world\n");
+<<<<<<< HEAD
+	printf("1111111\n");
+	printf("22222\n");
+	printf("3333\n");
+=======
+>>>>>>> fa26184ef3c8944f6752649cfa37495501dc403a
+	printf("hello develop\n");
+	return 0;
+}
